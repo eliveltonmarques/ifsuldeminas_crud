eeacc273746d35a6d41bb7dad8670dee382ad6e0
eliveltonmarques <eliveltonmsts@gmail.com>
2023-07-27 19:12:49 -0300

    Semana 5

--- app/Http/Controllers/Produto.php
+++ app/Http/Controllers/Produto.php
@@ -1,11 +1,23 @@
 <?php
 
+
 namespace App\Models;
 
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 use Illuminate\Database\Eloquent\Model;
 
-class Produtos extends Model
+
+class Produto extends Model
+
 {
+
     use HasFactory;
+
+    protected $fillable = [
+
+        'descricao', 'qtd', 'precoUnitario', 'precoVenda'
+
+    ];
 }
